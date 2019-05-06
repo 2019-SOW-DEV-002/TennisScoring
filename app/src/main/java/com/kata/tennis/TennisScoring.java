5ad494c1c53971521db9f04ac82eaa7a6d818b33
2019_SOW_DEV_002 <2019_SOW_DEV_002@bnpparibasfortis.com>
2019-05-06 11:59:33 +0200

    Add Android project with Tennis class and its test class

---
+++ app/src/main/java/com/kata/tennis/TennisScoring.java
@@ -0,0 +1,13 @@
+package com.kata.tennis;
+
+public class TennisScoring {
+    private int player1;
+
+    public int getPlayer1Score() {
+        return player1;
+    }
+
+    public void addPointForPlayer1() {
+        player1++;
+    }
+}
---
+++ app/src/test/java/com/kata/tennis/TennisScoringTest.java
@@ -0,0 +1,17 @@
+package com.kata.tennis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TennisScoringTest {
+
+    @Test
+    public void shouldAddScoreForPlayer1_When_Player1Scores1Point() {
+        TennisScoring tennisScoring = new TennisScoring();
+
+        tennisScoring.addPointForPlayer1();
+
+        assertEquals(1, tennisScoring.getPlayer1Score());
+    }
+}
